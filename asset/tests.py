e370aa4ff8018b3d11f6fc1bc07b9c50549b780f
brenorosas <brenorosas@hotmail.com>
2021-04-19 14:29:47 -0300

    tests login, logout and home

--- asset/tests.py
+++ asset/tests.py
@@ -18,3 +18,38 @@ class AssetTestCase(TestCase):
     def test_retorno_str(self):
         as1 = Asset.objects.get(name = 'NameTest')
         self.assertEquals(as1.__str__(), 'NameTest')
+
+#views
+class LoginViewTestCase(TestCase):
+    def test_status_code_200(self):
+        response = self.client.get(reverse('login'))
+        self.assertEquals(response.status_code, 200)
+    
+    def test_template_used(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'login.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_post(self):
+        us = User.objects.create(username = 'test')
+        us.set_password('test')
+        us.save()
+        response = self.client.post(reverse('login'), data = {'username' : 'test', 'password' : 'test'}, follow = True)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 200)
+        us.delete()
+
+class HomeViewTestCase(TestCase):
+    def test_status_code_200(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'home.html')
+
+class LogoutViewTestCase(TestCase):
+    def test_status_code_302(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEquals(response.status_code, 302)
