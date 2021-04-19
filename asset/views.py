5d1a90ce54089255a89f4b4635b56363e518f0a8
brenorosas <brenorosas@hotmail.com>
2021-04-19 13:49:18 -0300

    login and logout

--- asset/views.py
+++ asset/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from django.views.generic import View, TemplateView, CreateView, ListView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+from django.contrib.auth import logout, login
+from django.views.generic import View, TemplateView, CreateView, ListView, RedirectView
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
 # Create your views here.
-class TemplateHomeView(TemplateView):
+class HomeView(TemplateView):
     template_name = 'home.html'
 
-class TemplateLoginView(Viwe):
-     template_name = 'login.html'
+class LoginView(RedirectView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form' : form})
+
+    def post(self, request):
+        form = self.form_class(data = request.POST)
+        if(form.is_valid()):
+            login(request, form.get_user())
+            return redirect('home')
+        form = self.form_class()
+        return render(request, self.template_name, {'form' : form})
+    
+class LogoutView(RedirectView):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
+
+
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from asset import views
+from asset.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.TemplateHomeView.as_view(), name = 'home'),
+    path('', HomeView.as_view(), name = 'home'),
+    path('login/', LoginView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(), name = 'logout'),
 ]
+
