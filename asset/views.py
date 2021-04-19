c21252a6031d8185851f48b19d2aa313d4d5ae91
brenorosas <brenorosas@hotmail.com>
2021-04-19 12:20:03 -0300

    home view

--- asset/views.py
+++ asset/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import View, TemplateView, CreateView, ListView
+from .models import *
 # Create your views here.
+class TemplateHomeView(TemplateView):
+    template_name = 'home.html'
+
+class TemplateLoginView(Viwe):
+     template_name = 'login.html'
